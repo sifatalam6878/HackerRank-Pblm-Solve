857aeaf3898ef1fe32a63b5921652bb5492d2843
Sifat Alam <sifaterabbi20@gmail.com>
2025-11-13 00:57:57 +0600

    hacker rank pblm solve c++

---
+++ all pblm solve/For_Loop.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+
+    string num[]={"", "one","two","three",
+        "four","five","six","seven","eight","nine"};
+    for(int i=a;i<=b;i++)
+    {
+        if(i<=9)
+            cout << num[i] << endl;
+        else
+            cout << (i%2 == 0 ? "even" : "odd") << endl;
+    }
+    return 0;
+}
